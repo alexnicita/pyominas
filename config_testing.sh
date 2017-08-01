f4728f70eb4114042551fccedebb2a7ff13615c2
Alex Nicita <an2582@columbia.edu>
2017-08-01 10:02:41 -0700

    finalize launch script

--- config_testing.sh
+++ config_testing.sh
@@ -4,4 +4,5 @@ export PATH="/data/software/anaconda2/bin:$PATH"
 export LD_LIBRARY_PATH="/data/software/anaconda2/lib/:/usr/local/exelis/idl85/bin/bin.linux.x86_64"
 export PYTHONPATH="/usr/local/exelis/idl85/bin/bin.linux.x86_64:/usr/local/exelis/idl85/lib/bridges"
 . ~/.ominas/ominas_setup.sh
-exec python
+
+python $@
