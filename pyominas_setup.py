951faaf0aa112e1bdaf5703513edea664040b527
Alex Nicita <an2582@columbia.edu>
2017-08-16 14:49:40 -0700

    need to test module imports

--- pyominas_setup.py
+++ pyominas_setup.py
@@ -1,7 +1,7 @@
 # executed in python environment on launch of linux_pyominas
-
 import sys
 import os
 sys.path.insert(1, os.environ['PYOMINAS_DIR']) # receives environment variable from pyominas' configure.sh
 from idlpy import *
-from ominas import ominas
+
+#### could 'import idlpy as ominas' be an alternative to a metaclass inside idlpy.py???
