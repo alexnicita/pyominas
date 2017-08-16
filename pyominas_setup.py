69d5e0fdfff40ef233d66284e0d1dbb60f88356f
Alex Nicita <an2582@columbia.edu>
2017-08-16 13:48:20 -0700

    implement checks for PYOMINAS_DIR, PYTHONPATH, alias

--- pyominas_setup.py
+++ pyominas_setup.py
@@ -1,5 +1,7 @@
+# executed in python environment on launch of linux_pyominas
+
 import sys
 import os
-sys.path.append(os.environ['PYOMINAS_DIR'])
+sys.path.insert(1, os.environ['PYOMINAS_DIR']) # receives environment variable from pyominas' configure.sh
 from idlpy import *
 from ominas import ominas
