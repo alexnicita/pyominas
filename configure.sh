91547f066c7a27b6ecc5599d3d94e61777ca6f29
Alex Nicita <an2582@columbia.edu>
2017-08-16 16:41:17 -0700

    ubuntu dash workaround

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 ###############################################################################
 ### configure.sh script for pyominas
 ### meant to be run after repository is cloned, but before use
